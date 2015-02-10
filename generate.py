06cdd50a64d7c9e8a6567fc45e471fa56f7881d8
Matt Soucy <msoucy@csh.rit.edu>
2015-02-10 01:32:28 -0500

    Argument parsing

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 from __future__ import print_function
+import argparse
 import hashlib
 from itertools import ifilter
 from jinja2 import Environment, FileSystemLoader
@@ -32,13 +33,31 @@ def loadyaml(name):
 def yaml_filter(role):
     return lambda yml: yml.get('role', 'student') == role
 
-template = get_template('template.html')
-names = rootedlistdir("profiles")
-yamls = [loadyaml(name) for name in names]
-ROLES = ["captain", "faculty", "student", "alumni"]
-sortedyamls = (yml for role in ROLES
-               for yml in filter(yaml_filter(role), yamls))
-rendered = (template.render(person=yml) for yml in sortedyamls)
 
-with open("output.html", "w") as f:
-    print("\n".join(rendered), file=f)
+def parse_arguments():
+    ROLES = ["captain", "faculty", "student", "alumni"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--template', '-t', default='template.html',
+                        help='Jinja template to use')
+    parser.add_argument('--roles', '-r', nargs='+',
+                        default=ROLES, help='Ordered list of roles to use')
+    parser.add_argument('--output', '-o', default='-',
+                        type=argparse.FileType('w'),
+                        help='Output filename')
+    parser.add_argument('directory', help='Directory to read through')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    template = get_template(args.template)
+    names = rootedlistdir(args.directory)
+    yamls = [loadyaml(name) for name in names]
+    sortedyamls = (yml for role in args.roles
+                   for yml in filter(yaml_filter(role), yamls))
+    rendered = (template.render(person=yml) for yml in sortedyamls)
+
+    print("\n".join(rendered), file=args.output)
+
+if __name__ == '__main__':
+    main()
