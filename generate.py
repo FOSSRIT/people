126b7fcd572e38516748209c4c6cb5451050bd2f
Matt Soucy <msoucy@csh.rit.edu>
2015-02-10 00:24:00 -0500

    Flat is better than nested

--- generate.py
+++ generate.py
@@ -1,22 +1,44 @@
+#!/bin/env python
 from __future__ import print_function
 import hashlib
+from itertools import ifilter
 from jinja2 import Environment, FileSystemLoader
 import os
 import yaml
 
-with open("template.html") as f:
+
+def get_template(name):
     env = Environment(loader=FileSystemLoader('.'))
 
-def testbio(bio):
-    assert len(bio) < 140
-    return bio
+    def testbio(bio):
+        assert len(bio) <= 140, "Biography is too long"
+        return bio
 
-env.filters['md5sum'] = lambda value: hashlib.md5(value).hexdigest()
-env.filters['testbio'] = testbio
+    env.filters['md5sum'] = lambda value: hashlib.md5(value).hexdigest()
+    env.filters['testbio'] = testbio
 
-template = env.get_template('template.html')
+    return env.get_template(name)
+
+
+def rootedlistdir(pth):
+    return [os.path.join(pth, name) for name in os.listdir(pth)]
+
+
+def loadyaml(name):
+    with open(name) as inf:
+        return yaml.load(inf)
+
+
+def yaml_filter(role):
+    return lambda yml: yml.get('role', 'student') == role
+
+template = get_template('template.html')
+names = rootedlistdir("profiles")
+yamls = [loadyaml(name) for name in names]
+ROLES = ["captain", "faculty", "student", "alumni"]
+sortedyamls = (yml for role in ROLES
+               for yml in filter(yaml_filter(role), yamls))
+rendered = (template.render(person=yml) for yml in sortedyamls)
 
 with open("output.html", "w") as f:
-    for yml in os.listdir("profiles"):
-        with open(os.path.join("profiles", yml)) as inf:
-            print(template.render(person=yaml.load(inf)), file=f)
+    print("\n".join(rendered), file=f)
